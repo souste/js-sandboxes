6220e541b1b0b1edea24a6708d1027edd8683ab8
Stephen <stephensouness@outlook.com>
2026-05-19 18:18:42 +0100

    feat(spells): add search bar

--- 01-dnd-api-fetch/src/Spells.jsx
+++ 01-dnd-api-fetch/src/Spells.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 export default function Spells() {
   const [spells, setSpells] = useState([]);
   const [selectedSpell, setSelectedSpell] = useState(null);
+  const [searchValue, setSearchValue] = useState("");
 
   useEffect(() => {
     const fetchSpells = async () => {
@@ -24,23 +25,44 @@ export default function Spells() {
     setSelectedSpell(null);
   };
 
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+  };
+
   return (
-    <div>
+    <div className="search-container">
+      <label>Search for your favourite spells here</label>
+      <input
+        type="text"
+        value={searchValue}
+        placeholder="Search Spells"
+        onChange={handleChange}
+      />
+      <p>Current Value: {searchValue}</p>
       <ul>
-        {spells.slice(0, 10).map((spell) => (
-          <li onClick={() => generateSpellDetails(spell.url)} key={spell.index}>
-            {spell.name}
-          </li>
-        ))}
+        {spells
+          .filter((spell) =>
+            spell.name.toLowerCase().includes(searchValue.toLowerCase()),
+          )
+          .slice(0, 100)
+          .map((spell) => (
+            <li
+              onClick={() => generateSpellDetails(spell.url)}
+              key={spell.index}
+            >
+              {spell.name}
+            </li>
+          ))}
       </ul>
 
       <div>
         {selectedSpell && (
-          <div onClick={() => closeSpellDetails()}>
+          <div>
             <p>{selectedSpell.name}</p>
             <p>{selectedSpell.range}</p>
             <p>{selectedSpell.level}</p>
             <p>{selectedSpell.desc?.join(" ")}</p>
+            <button onClick={() => closeSpellDetails()}>Close</button>
           </div>
         )}
       </div>
