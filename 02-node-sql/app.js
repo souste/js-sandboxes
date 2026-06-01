dc90c7968691b3d21676a4047ede8336a399815f
Stephen <stephensouness@outlook.com>
2026-06-01 15:06:46 +0100

    feat: initial setup and add express server

---
+++ 02-node-sql/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.json({ message: "Hello to the SQL sandbox" }));
+
+const PORT = 3000;
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Listening on port ${PORT}`);
+});
