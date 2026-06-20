155109c65375ff56d30d424ec7a73090ca08362f
Stephen <stephensouness@outlook.com>
2026-06-20 12:04:18 +0100

    feat(ShoppingCartTest): add item and category select filtering

--- 03-react-test-lab/src/ShoppingCartTest.jsx
+++ 03-react-test-lab/src/ShoppingCartTest.jsx
@@ -41,11 +41,39 @@ const INITIAL_CART = [
 
 export function ShoppingCartTest() {
   const [items, setItems] = useState(INITIAL_CART);
+  const [itemSelectValue, setItemSelectValue] = useState("all");
+  const [categorySelectValue, setCategorySelectValue] = useState("all");
+
+  const filteredItems = items
+    .filter((item) => itemSelectValue === "all" || item.id === itemSelectValue)
+    .filter(
+      (item) =>
+        categorySelectValue === "all" || item.category === categorySelectValue,
+    );
+
   return (
     <div>
       <div>
-        {items.map((item) => (
-          <div>
+        <p>Filter items: </p>
+        <select onChange={(e) => setItemSelectValue(e.target.value)}>
+          <option value="all">All</option>
+          <option value="cart-item-1">Wireless Controller</option>
+          <option value="cart-item-2">Keyboard Switch Set</option>
+          <option value="cart-item-3">Laptop Cooling Stand</option>
+          <option value="cart-item-4">Nylon USB-C Cable</option>
+        </select>
+        <p>Filter Categories</p>
+        <select onChange={(e) => setCategorySelectValue(e.target.value)}>
+          <option value="all">All</option>
+          <option value="Gaming Gears">Gaming Gears</option>
+          <option value="PC Components">PC Components</option>
+          <option value="Office Comfort">Office Comfort</option>
+          <option value="Accessories">Accessories</option>
+        </select>
+      </div>
+      <div>
+        {filteredItems.map((item) => (
+          <div key={item.id}>
             <strong>{item.name}</strong>
             <p>Category: {item.category}</p>
             <p>Price: £{item.price}</p>
