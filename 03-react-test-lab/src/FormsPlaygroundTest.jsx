90f9c2df8e20045fbca141837e59d28c446c7009
Stephen <stephensouness@outlook.com>
2026-06-30 14:03:40 +0100

    feat(FormsPlaygroundTest): implement error handling and form validation

--- 03-react-test-lab/src/FormsPlaygroundTest.jsx
+++ 03-react-test-lab/src/FormsPlaygroundTest.jsx
@@ -8,22 +8,57 @@ export const FormsPlaygroundTest = () => {
     password: "",
   });
   const [successPage, setSuccessPage] = useState(false);
+  const [errors, setErrors] = useState({});
 
   const handleChange = (event) => {
     const { name, value } = event.target;
-
     setFormValues((prev) => ({ ...prev, [name]: value }));
+
+    setSuccessPage(false);
+
+    if (errors[name]) {
+      setErrors((prev) => ({ ...prev, [name]: "" }));
+    }
+  };
+
+  const validateForm = () => {
+    let tempErrors = {};
+
+    if (!formValues.firstname.trim())
+      tempErrors.firstname = "First name is required";
+
+    if (!formValues.surname.trim()) tempErrors.surname = "Surname is required";
+
+    const emailPattern = /\S+@\S+\.\S+/;
+    if (!formValues.email) {
+      tempErrors.email = "Email is required";
+    } else if (!emailPattern.test(formValues.email)) {
+      tempErrors.email = "Please enter a valid email address";
+    }
+
+    if (!formValues.password) {
+      tempErrors.password = "A password is required";
+    } else if (formValues.password.length < 8) {
+      tempErrors.password = "Password must be at least 8 characters long";
+    }
+    setErrors(tempErrors);
+
+    return Object.keys(tempErrors).length === 0;
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setSuccessPage(true);
+    if (validateForm()) {
+      setSuccessPage(true);
+    } else {
+      setSuccessPage(false);
+    }
   };
 
   return (
     <div>
       {successPage && (
-        <div>
+        <div className="success-page-container">
           <p>Signup Successful. Here are your details:</p>
           <p>
             Name: {formValues.firstname} {formValues.surname}
@@ -33,8 +68,9 @@ export const FormsPlaygroundTest = () => {
         </div>
       )}
 
-      <form onSubmit={handleSubmit} className="form-container">
+      <form onSubmit={handleSubmit} noValidate className="form-container">
         <label>Firstname:</label>
+        {errors.firstname && <p className="errors">{errors.firstname}</p>}
         <input
           type="text"
           name="firstname"
@@ -43,6 +79,7 @@ export const FormsPlaygroundTest = () => {
           placeholder="Jason"
         />
         <label>Surname:</label>
+        {errors.surname && <p className="errors">{errors.surname}</p>}
         <input
           type="text"
           name="surname"
@@ -51,6 +88,7 @@ export const FormsPlaygroundTest = () => {
           placeholder="Duval"
         />
         <label>Email:</label>
+        {errors.email && <p className="errors">{errors.email}</p>}
         <input
           type="email"
           name="email"
@@ -59,6 +97,7 @@ export const FormsPlaygroundTest = () => {
           placeholder="jasonduval@gtamail.com"
         />
         <label>Password:</label>
+        {errors.password && <p className="errors">{errors.password}</p>}
         <input
           type="password"
           name="password"
