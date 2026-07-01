be9c1c5f7b9840e6d2af120160f86f7473911d5f
Stephen <stephensouness@outlook.com>
2026-07-01 12:43:27 +0100

    feat(04-node-playground): initial setup and basic server

---
+++ 04-node-playground/src/app.js
@@ -0,0 +1,19 @@
+// require("dotenv").config()
+
+const express = require("express");
+// const pool = require("./db/pool")
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) =>
+  res.json({ message: "Welcome to the Node Playground" }),
+);
+
+const PORT = 3000;
+
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Listening on port ${PORT}`);
+});
