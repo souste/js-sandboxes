e857b22878a8ffb65dc780c2c9931461aec1d1cf
Stephen <stephensouness@outlook.com>
2026-07-24 12:49:37 +0100

    feat(crm-app): Add contact sorting functionality

--- 05-crm-app/client/src/components/ContactsList.jsx
+++ 05-crm-app/client/src/components/ContactsList.jsx
@@ -8,11 +8,13 @@ function ContactsList() {
   const [contacts, setContacts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [searchTerm, setSearchTerm] = useState("");
+  const [sortBy, setSortBy] = useState("none");
 
   useEffect(() => {
     const fetchContacts = async () => {
       try {
         const contacts = await getContacts();
+        console.log(contacts);
         setContacts(contacts);
       } catch (err) {
         console.error("Failed to fetch contacts", err);
@@ -26,13 +28,27 @@ function ContactsList() {
 
   const search = searchTerm.toLowerCase();
 
-  const displayedContacts = contacts.filter((contact) => {
-    return (
-      contact.first_name.toLowerCase().includes(search) ||
-      contact.surname.toLowerCase().includes(search) ||
-      contact.company_name?.toLowerCase().includes(search)
-    );
-  });
+  const displayedContacts = contacts
+    .filter((contact) => {
+      return (
+        contact.first_name.toLowerCase().includes(search) ||
+        contact.surname.toLowerCase().includes(search) ||
+        contact.company_name?.toLowerCase().includes(search)
+      );
+    })
+    .sort((a, b) => {
+      console.log(sortBy);
+      if (sortBy === "name-a-z")
+        return a.first_name.localeCompare(b.first_name);
+      if (sortBy === "name-z-a")
+        return b.first_name.localeCompare(a.first_name);
+      if (sortBy === "company-asc")
+        return (a.company_name || "").localeCompare(b.company_name || "");
+      if (sortBy === "industry-asc")
+        return (a.industry || "").localeCompare(b.industry || "");
+
+      return 0;
+    });
 
   if (loading) return <p>Loading Contacts</p>;
 
@@ -50,6 +66,16 @@ function ContactsList() {
           onChange={(e) => setSearchTerm(e.target.value)}
           placeholder="Search for any contact"
         />
+      </div>
+      <div>
+        <label>Sort Cotacts</label>
+        <select onChange={(e) => setSortBy(e.target.value)}>
+          <option value="none">Default</option>
+          <option value="name-a-z">First Name A-Z</option>
+          <option value="name-z-a">First Name Z-A</option>
+          <option value="company-asc">Company A-Z</option>
+          <option value="industry-asc">Industry A-Z</option>
+        </select>
       </div>
       <div>
         {displayedContacts.length === 0 ? (
